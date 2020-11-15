57543f7d0d919b64c7e27be0f3a8c2d986d8d370
afifym <mohamed.afify323@gmail.com>
2020-11-15 12:59:23 +0200

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 1200,
+    reset: true
+});
+
+sr.reveal('#navigation',{}); 
+
+sr.reveal('#profile',{delay: 100}); 
+sr.reveal('#skills',{delay: 100}); 
+sr.reveal('#projects',{delay: 100}); 
+// sr.reveal('.contact',{delay: 100}); 
